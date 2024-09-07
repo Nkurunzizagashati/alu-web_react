7436392d3c6a71dd34db34e2f45d37108b0d102b
Fabrice <f.nkurunziz@alustudent.com>
2024-09-07 17:29:26 +0200

    webpack

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,52 +1,29 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './js/dashboard_main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public'),
-		clean: true,
-	},
-	mode: 'production',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html', // Template for index.html
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|jpg|jpeg|gif)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'assets/',
-						},
-					},
-					{
-						loader: 'image-webpack-loader', // Image optimization
-						options: {
-							mozjpeg: {
-								progressive: true,
-							},
-							optipng: {
-								enabled: true,
-							},
-							pngquant: {
-								quality: [0.65, 0.9],
-								speed: 4,
-							},
-						},
-					},
-				],
-			},
-		],
-	},
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                use: ['file-loader', {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        bypassOnDebug: true,
+                        disable: true
+                    },
+                },]
+            }
+        ],
+    },
 };
