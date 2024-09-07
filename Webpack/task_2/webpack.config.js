83317c7301439ffa1ff1dde1b31672d177cd4462
Fabrice <f.nkurunziz@alustudent.com>
2024-09-07 17:58:01 +0200

    webpack

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	mode: 'production',
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './public/index.html', // Template for index.html
+			template: './public/index.html',
 		}),
 	],
 	module: {
