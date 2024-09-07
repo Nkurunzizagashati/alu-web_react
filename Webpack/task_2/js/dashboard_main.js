68b8c525c0679ca906e71fcb04b40efff7ba41df
Fabrice <f.nkurunziz@alustudent.com>
2024-09-07 17:41:31 +0200

    webpack

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,17 +1,17 @@
-import $ from "jquery";
-import _ from "lodash";
+import $ from 'jquery';
+import '../css/main.css'; // Import the CSS
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append(
+	'<button id="clickButton">Click here to get started</button>'
+);
+$('body').append('<p id="count">0 clicks on the button</p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
+// Button click handler to update the counter
 let count = 0;
-
-function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-}
-
-$('button').on('click', _.debounce(updateCounter, 500));
+$('#clickButton').click(() => {
+	count++;
+	$('#count').text(`${count} clicks on the button`);
+});
